a26e9e9001c8c697b9c4756c74266da352602fa5
sytzez <sytzez@gmail.com>
2023-03-08 08:39:38 +0000

    Show comments and status updates on conversation view

--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -1,4 +1,6 @@
 class StatusUpdate < ApplicationRecord
+  default_scope { includes(:conversation_item) }
+
   enum status: {
     proposed: 0,
     planned: 1,
@@ -17,4 +19,17 @@ class StatusUpdate < ApplicationRecord
   delegate :created_at, :author, to: :conversation_item
 
   validates :status, presence: true
+
+  # The status update just before this one. Can be nil.
+  def previous_status_update
+    self.class
+        .where('conversation_items.created_at < ?', conversation_item.created_at)
+        .order('conversation_items.created_at': :desc)
+        .first
+  end
+
+  # The status just before this one. Can be nil.
+  def previous_status
+    previous_status_update&.status
+  end
 end
